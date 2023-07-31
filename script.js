dd7a134ed9d22d7571dc567baf8c8409ad12206d
myr2212 <124097260+myr2212@users.noreply.github.com>
2023-08-01 05:07:34 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,37 @@
-function predictSpecies() {
-    const length1 = parseFloat(document.getElementById("length1").value);
-    const length2 = parseFloat(document.getElementById("length2").value);
-    const length3 = parseFloat(document.getElementById("length3").value);
-    const height = parseFloat(document.getElementById("height").value);
-    const width = parseFloat(document.getElementById("width").value);
+function predictFishSpecies() {
+  // Get the input values
+  var length1 = parseFloat(document.getElementById("length1").value);
+  var length2 = parseFloat(document.getElementById("length2").value);
+  var length3 = parseFloat(document.getElementById("length3").value);
+  var height = parseFloat(document.getElementById("height").value);
+  var width = parseFloat(document.getElementById("width").value);
+  
+  // Create the input data object
+  var inputData = {
+    "Length1": length1,
+    "Length2": length2,
+    "Length3": length3,
+    "Height": height,
+    "Width": width
+  };
 
-    // Here you can perform any data validation or preprocessing as needed
-
-    // Make a request to the server to get the prediction
-    // You can use Fetch API or any other library for this purpose
-    // For this example, let's assume we already have the predicted species from the model
-    const predictedSpecies = "Bream";
-
-    document.getElementById("prediction").innerText = `Predicted Species: ${predictedSpecies}`;
+  // Send a POST request to the Flask backend
+  fetch("/predict", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(inputData)
+  })
+  .then(response => response.json())
+  .then(data => {
+    // Display the predicted species
+    var outputContainer = document.getElementById("output-container");
+    var predictedSpecies = document.getElementById("predicted-species");
+    predictedSpecies.textContent = "Predicted Species: " + data.predicted_species;
+    outputContainer.style.display = "block";
+  })
+  .catch(error => {
+    console.error("Error:", error);
+  });
 }
